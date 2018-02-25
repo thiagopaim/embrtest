88c4e49ad9da4cc5876042ba341213756ac6403f
Thiago Paim <thiagopaim@msn.com>
2018-02-25 11:45:48 -0300

    update to PUG

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,64 @@
-// npm install gulp
-// npm install gulp-concat gulp-jade gulp-sass gulp-plumber gulp-cssnano gulp-uglify gulp-autoprefixer gulp-rename gulp-watch browser-sync
+const   gulp            = require('gulp'),
+        concat          = require('gulp-concat'),
+        pug             = require('gulp-pug'),
+        sass            = require('gulp-sass'),
+        plumber         = require('gulp-plumber'),
+        minifyCSS       = require('gulp-cssnano'),
+        uglifyJS        = require('gulp-uglify'),
+        autoprefixer    = require('gulp-autoprefixer'),
+        rename          = require('gulp-rename'),
+        jsImport        = require('gulp-js-import'),
+        imageMin        = require('gulp-imagemin'),
+        watch           = require('gulp-watch'),
+        browserSync     = require('browser-sync');
 
-var gulp            = require('gulp'),
-    concat          = require('gulp-concat'),
-    jade            = require('gulp-jade'),
-    sass            = require('gulp-sass'),
-    plumber         = require('gulp-plumber'),
-    minifyCSS       = require('gulp-cssnano'),
-    uglifyJS        = require('gulp-uglify'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    rename          = require('gulp-rename'),
-    watch           = require('gulp-watch'),
-    browserSync     = require('browser-sync');
-
-// compila jade
-gulp.task('templates', function () {
-  return gulp.src('client/**/*.jade')
-    .pipe(jade({
-        client: false,
-        pretty: true
-    }))
-    .pipe(gulp.dest('./build/'))
-})
+// compila pug
+gulp.task('templates', () =>
+    gulp.src('./client/**/*.pug')
+        .pipe(plumber())
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest('./build/'))
+);
 
 // minifica e concatena folha de estilos
-gulp.task('styles', function(){
-    return gulp.src([
-        './client/sass/style.scss',
-    ])
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(minifyCSS({
-        keepBreaks: false
-    }))
-    .pipe(concat('build.css'))
-    .pipe(gulp.dest('build/'));
-});
+gulp.task('styles', () =>
+    gulp.src(['./client/sass/style.scss'])
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(minifyCSS({ keepBreaks: false }))
+        .pipe(concat('build.css'))
+        .pipe(gulp.dest('./build/'))
+);
 
 // minifica e concatena javascript
-gulp.task('js', function(){
- return gulp.src(['./client/js/**/*.js'])
-    .pipe(plumber())
-    .pipe(uglifyJS({mangle: false}))
-    .pipe(concat('build.js'))
-    .pipe(gulp.dest('build/'))
-});
+gulp.task('scripts', () =>
+    gulp.src(['./client/js/**/*.js'])
+        .pipe(plumber())
+        .pipe(jsImport({ hideConsole: true }))
+        .pipe(uglifyJS({ mangle: false }))
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('./build/'))
+);
+
+gulp.task('images', () =>
+    gulp.src('./src/images/*')
+        .pipe(imageMin())
+        .pipe(gulp.dest('./build/images/'))
+);
 
 // browser releaod
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', () =>
     browserSync({
         server: {
             baseDir: './build/'
         }
-    });
-});
+    })
+);
 
 gulp.task('default', ['browser-sync'], function() {
-    gulp.watch('./client/**/*.jade', ['templates', browserSync.reload]);
-    gulp.watch('./client/sass/**/*.scss', ['styles', browserSync.reload]);
-    gulp.watch('./client/js/**/*.js', ['js', browserSync.reload]);
+    gulp.watch('./client/**/*.pug', ['templates', browserSync.reload])
+    gulp.watch('./client/styles/**/*.scss', ['styles', browserSync.reload])
+    gulp.watch('./client/scripts/**/*.js', ['scripts', browserSync.reload])
+    gulp.watch('./client/images/*', ['images', browserSync.reload])
 });
+
